f49b11388f6e64962f6df69c90c1ccdda4b5d398
Marcus Ureta <silvergreat0@gmail.com>
2025-05-05 12:57:12 +0800

    initial commit

---
+++ Inventory System/View/User Controls/MenuBar.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Inventory_System.View.User_Controls
+{
+    public partial class MenuBar : UserControl
+    {
+        public MenuBar()
+        {
+            InitializeComponent();
+        }
+
+        private void Exit_Application(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Do you want to Exit the Application?", "Exit App", MessageBoxButton.YesNo);
+
+            switch (result)
+            {
+                case MessageBoxResult.Yes:
+                    Application.Current.Shutdown();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Programming Languages Q4-QA : Inventory System
+
