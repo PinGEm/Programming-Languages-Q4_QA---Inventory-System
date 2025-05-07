c21daeeeac31f165e216d2c8718e2441d8c14ed6
Marcus Ureta <silvergreat0@gmail.com>
2025-05-07 11:45:18 +0800

    data added

--- Inventory System/MainWindow.xaml.cs
+++ Inventory System/MainWindow.xaml.cs
@@ -4,13 +4,19 @@ namespace Inventory_System
 {
     public partial class MainWindow : Window
     {
+        public List<string> items = new List<string>();
+
         public MainWindow()
         {
             InitializeComponent();
 
             var MenuBar = this.menuBar;
+            var AddItem = this.addItem;
+            var ViewInventory = this.viewInventory;
 
             menuBar.FindMainWindow(this);
+            addItem.FindMainWindow(this);
+            viewInventory.FindMainWindow(this);
         }
     }
 }
--- Inventory System/View/User Controls/AddItem.xaml.cs
+++ Inventory System/View/User Controls/AddItem.xaml.cs
@@ -5,6 +5,14 @@ namespace Inventory_System.View.User_Controls
 
     public partial class AddItem : UserControl
     {
+        MainWindow mainWindow;
+        string input_text;
+        
+        public void FindMainWindow(MainWindow main)
+        {
+            mainWindow = main;
+        }
+
         public AddItem()
         {
             InitializeComponent();
@@ -12,12 +20,25 @@ namespace Inventory_System.View.User_Controls
 
         private void SubmitItem(object sender, System.Windows.RoutedEventArgs e)
         {
-            string input_text = itemName.Text;
+            string Input_text = itemName.Text;
 
-            if (!string.IsNullOrEmpty(input_text))
+            if (!string.IsNullOrEmpty(Input_text))
             {
+                int index = -1;
+
                 // do something
-                confirmationText.Text = input_text + " has been added to your inventory!";
+                confirmationText.Text = Input_text + " has been added to your inventory!";
+                input_text = Input_text;
+                mainWindow.items.Add(input_text);
+
+                //TEMPORARY
+                for (int i = -1; i < mainWindow.items.Count; i++)
+                {
+                    index = i;
+                }
+
+                mainWindow.viewInventory.index_IL.Items.Add(index);
+                mainWindow.viewInventory.name_IL.Items.Add(mainWindow.items[index]);
             }
             else
             {
